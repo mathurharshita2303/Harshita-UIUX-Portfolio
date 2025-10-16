02503adc4eeb7281d7d2bab4c494e1a689641537
Harshita.Mathur <harshita.mathur@inspiritvision.com>
2025-10-16 09:41:45 +0530

    netlify ready

--- Harshita_Portfolio/src/config.js
+++ Harshita_Portfolio/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5002";
-export default API_BASE_URL;
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5002";
+export default API_BASE;
